aa7d4530d1a654e347a8730a3b3576f793153321
gustavo.nunes <gustavonunes392@gmail.com>
2025-04-23 05:17:52 -0300

    fix: consertando bug de acesso ao swagger pos docker

--- backend/src/main/java/com/voidGustavoNunes/Accenture/config/SwaggerConfig.java
+++ backend/src/main/java/com/voidGustavoNunes/Accenture/config/SwaggerConfig.java
@@ -1,18 +1,28 @@
 package com.voidGustavoNunes.Accenture.config;
 
 
+import java.util.List;
+
+import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 
 @Configuration
 public class SwaggerConfig {
     
     @Bean
-    public OpenAPI customApi(){
-        return new OpenAPI().info(new Info().title("Projeto Accenture").version("1.0.0")
-                                    .description("Projeto Desafio Accenture"));
+    public OpenAPI customApi() {
+        return new OpenAPI()
+            .info(new Info()
+                .title("Projeto Accenture")
+                .version("1.0.0")
+                .description("Projeto Desafio Accenture"))
+            .servers(List.of(
+                new Server().url("/").description("Current server")
+            ));
     }
 }
